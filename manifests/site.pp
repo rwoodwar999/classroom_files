91cfa798fd4cada9368b7e3722e66ce107f90178
Joseph Oaks <josephoaks@users.noreply.github.com>
2017-03-29 10:33:27 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,13 @@
 ## site.pp ##
 
+# Exercise 7.1
 node 'josephoaks.puppetlabs.vm' {
   include system_users::admins
 }
 
 node default {
   include role::classroom
+  
+  # Exercise 6.2
   include system_users
 }
