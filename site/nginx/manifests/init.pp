e28ae2ceedd1c2608ec020066493ee353ab8fdbd
rwoodwar999 <rwoodwar@travelers.com>
2017-03-29 13:28:28 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -3,18 +3,37 @@ class nginx {
   ensure => present,
   }
 
-file { '/var/www':
-  ensure => directory,
-  owner => 'root',
-  group => 'root',
-  mode => '0775', 
-  }
+file { 'docroot':
+    ensure => directory,
+    path =>  '/var/www',
+    owner => '/root',
+    group => 'root',
+    }
 
-file { '/var/www/index.html':
+file { '/index.html':
   ensure => file,
+  path  =>  '/var/www/index.html'
   owner => 'root',
   group => 'root',
   mode => '0664', # allow Puppet to re-write files as needed on Windows
   source => 'puppet:///modules/nginx/index.html',
   }
+  file {'ngix.conf'"
+  {
+    ensure => file,
+    path  =>  '/var/www/ngix.conf'
+    owner => 'root',
+    group => 'root',
+    mode => '0664', # allow Puppet to re-write files as needed on Windows
+    source => 'puppet:///modules/nginx/ngix.conf',
+ 
+ file {'default.conf'"
+  {
+    ensure => file,
+    path  =>  '/var/www/default.conf'
+    owner => 'root',
+    group => 'root',
+    mode => '0664', # allow Puppet to re-write files as needed on Windows
+    source => 'puppet:///modules/nginx/default.conf',
+  
 }
