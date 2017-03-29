7bad7dde1c7d020591f7b0f6b26ff87d178b5016
rwoodwar999 <rwoodwar@travelers.com>
2017-03-29 13:15:58 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -9,25 +9,6 @@
 # http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
 # node definitions.)
 
-
-
-# Disable filebucket by default for all File resources:
-File { backup => false }
-
-# Randomize enforcement order to help understand relationships
-ini_setting { 'random ordering':
-  ensure  => present,
-  path    => "${settings::confdir}/puppet.conf",
-  section => 'agent',
-  setting => 'ordering',
-  value   => 'title-hash',
-}
-
-# DEFAULT NODE
-# Node definitions in this file are merged with node data from the console. See
-# http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
-# node definitions.
-
 # The default node definition matches any node lacking a more specific node
 # definition. If there are no other nodes in this file, classes declared here
 # will be included in every node's catalog, *in addition* to any classes
@@ -37,6 +18,8 @@ node 'rwoodwar999.puppetlabs.vm' {
   include system_users::admins
   #exercise 8.1
   include hosts::init
+  #exercise 8.2
+  include ngix::init
 }
 node default {
   # This is where you can declare classes for all nodes.
