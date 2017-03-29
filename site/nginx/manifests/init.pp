a11fd859e70c3cb4ccd51e757756b40c92874173
rwoodwar999 <rwoodwar@travelers.com>
2017-03-29 13:43:39 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -29,11 +29,12 @@ file { '/index.html':
  
  file {'default.conf':
     ensure => file,
-    path  =>  '/var/www/default.conf'
+    path  =>  '/var/www/default.conf',
     owner => 'root',
     group => 'root',
     mode => '0664', # allow Puppet to re-write files as needed on Windows
     source => 'puppet:///modules/nginx/default.conf',
+    }
   service {'nginx ':
     ensure  =>  running,
     enable  =>  true,
