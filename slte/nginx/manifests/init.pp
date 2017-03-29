6bf54588c1998fe4b87645a7137ea6c1c04f7425
msusman123 <msusman@travelers.com>
2017-03-29 13:28:35 -0400

    Create init.pp

---
+++ slte/nginx/manifests/init.pp
@@ -0,0 +1,41 @@
+class nginx {
+  package { 'nginx':
+    ensure  => present,
+  }
+
+  file { 'docroot':
+    ensure  =>  directory,
+    path    =>  '/var/www',
+    owner   =>  'root',
+    group   =>  'root'
+  }
+
+  file { 'index.html':
+    ensure  =>  file,
+    path    =>  '/var/www/index.html',
+    owner   =>  'root',
+    group   =>  'root',    
+    mode    =>  '0664',
+    source  =>  'puppet:///modules/nginx/index.html',
+  }
+
+  file { 'nginx.conf':
+    ensure  =>  file,
+    path    =>  '/var/www/nginx.conf',
+    owner   =>  'root',
+    group   =>  'root',    
+    mode    =>  '0664',
+    source  =>  'puppet:///modules/nginx/nginx.conf',
+  }default.conf
+
+  file { 'default.conf':  
+    ensure  =>  file,
+    path    =>  '/var/www/default.conf',
+    owner   =>  'root',
+    group   =>  'root',    
+    mode    =>  '0664',
+    source  =>  'puppet:///modules/nginx/default.conf',
+  }
+  
+  service { 'nginx'
+}
