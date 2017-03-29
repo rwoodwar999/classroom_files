fc8089c72207d87050cd103e6b3f54e0de3ce467
rwoodwar999 <rwoodwar@travelers.com>
2017-03-29 11:29:38 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -37,6 +37,9 @@ ini_setting { 'random ordering':
 # definition. If there are no other nodes in this file, classes declared here
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
+node 'rwoodwar999.puppetlabs.vm' {
+    include hosts::host
+  }
 
 node default {
   # This is where you can declare classes for all nodes.
