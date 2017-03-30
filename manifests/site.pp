9c2bd753d98549d0874a51dc5b38856600b0c3be
Joseph Oaks <josephoaks@users.noreply.github.com>
2017-03-30 13:23:04 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,8 +5,6 @@ node 'josephoaks.puppetlabs.vm' {
   include system_users::admins
   # Exercise 8.1
   include hosts
-  # Exercise 8.2
-  include nginx
   # Exercise 12.1
   nginx::vhost { 'punch.puppetlabs.vm': }
 }
