40565eba84a4f643f68ea6c164dd302acda7d436
Joseph Oaks <josephoaks@users.noreply.github.com>
2017-03-30 13:47:49 -0400

    Update configs.pp

--- site/nginx/manifests/configs.pp
+++ site/nginx/manifests/configs.pp
@@ -1,3 +1,36 @@
 class nginx::configs {
+  File {
+    ensure => file,
+    owner  => $owner,
+    group  => $group,
+    mode   => '0644',
+  }
 
+  file { 'docroot':
+    ensure => directory,
+    path   => $docroot,
+  }
+  
+  file { 'index.html':
+    path   => "${docroot}/index.html",    
+    content => epp('nginx/index.html.epp'),
+  }
+  
+  file { 'nginx.conf':
+    path   => "${confdir}/nginx.conf",
+    content => epp('nginx/nginx.conf.epp', {
+        user     => $user,
+        confdir  => $confdir,
+        blockdir => $blockdir,
+        logdir   => $logdir,
+        highperf => $highperf,
+      }),
+  }
+  
+  file { 'default.conf':
+    path   => "${blockdir}/default.conf",
+    content => epp('nginx/default.conf.epp', {
+        docroot => $docroot,
+      }),
+  }
 }
