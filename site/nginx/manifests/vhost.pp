bf41e92562ab23f3a598765bbb05ae0f18680220
Joseph Oaks <josephoaks@users.noreply.github.com>
2017-03-30 11:23:50 -0400

    Create vhost.pp

---
+++ site/nginx/manifests/vhost.pp
@@ -0,0 +1,34 @@
+class nginx::vhost (
+  $port       = '80',
+  $docroot    = "${nginx::docroot}/vhosts/${title}",
+  $servername = $title,
+) {
+
+  File {
+    ensure => file,
+    owner  => $nginx::owner,
+    group  => $nginx::group,
+    mode   => '0664',
+  }
+  
+  file { "nginx-vhost-${title}":
+    path    => "${nginx::blockdir}/${title}.conf",
+    content => epp(nginx/vhost.conf.epp', {
+      port       => $port,
+      docroot    => $docroot,
+      title      => $title,
+      servername => $servername,
+    }),
+    notify  => Service['nginx'],
+  }
+  
+  file { "${docroot}/index.html":
+    content => epp('nginx/index.html.epp', {
+      servername => $servername,
+    }),
+  }
+  
+  file { $docroot:
+    ensure => directory,
+  }
+}
