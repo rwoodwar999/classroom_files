e107ce15bf5e164788412e9b484d8330a6d764a3
Joseph Oaks <josephoaks@users.noreply.github.com>
2017-03-30 13:49:14 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -9,48 +9,7 @@ class nginx (
   $user     = $nginx::params::user,
 ) inherits nginx::params {
   
-  package { $package:
-    ensure => present,
-  }
-  
-  File {
-    ensure => file,
-    owner  => $owner,
-    group  => $group,
-    mode   => '0644',
-  }
-
-  file { 'docroot':
-    ensure => directory,
-    path   => $docroot,
-  }
-  
-  file { 'index.html':
-    path   => "${docroot}/index.html",    
-    content => epp('nginx/index.html.epp'),
-  }
-  
-  file { 'nginx.conf':
-    path   => "${confdir}/nginx.conf",
-    content => epp('nginx/nginx.conf.epp', {
-        user     => $user,
-        confdir  => $confdir,
-        blockdir => $blockdir,
-        logdir   => $logdir,
-        highperf => $highperf,
-      }),
-  }
-  
-  file { 'default.conf':
-    path   => "${blockdir}/default.conf",
-    content => epp('nginx/default.conf.epp', {
-        docroot => $docroot,
-      }),
-  }
-  
-  service { 'nginx':
-    ensure    => running,
-    enable    => true,
-    subscribe => [ File['nginx.conf'], File['default.conf'] ],
-  }
+  include nginx::packages
+  include nginx::configs
+  include nginx::services
 }
