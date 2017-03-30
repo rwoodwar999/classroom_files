bc719bef161068e79925eff4bff92d8d40163ab0
Joseph Oaks <josephoaks@users.noreply.github.com>
2017-03-30 09:33:26 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,32 +1,47 @@
 class nginx {
-  package { 'nginx':
+  case $facts['os']['family'] {
+    'redhat' : {
+      $package  = 'nginx'
+      $owner    = 'root'
+      $group    = 'root'
+      $docroot  = '/var/www'
+      $confdir  = '/etc/nginx'
+      $blockdir = '/etc/nginx/conf.d'
+      $logdir   = '/var/log/nginx'
+    }
+    default : {
+      fail("${module_name} is not supported on ${facts['os']['family']}")
+    }
+  }
+
+  package { $package:
     ensure => present,
   }
   
   File {
     ensure => file,
-    owner  => 'root',
-    group  => 'root',
+    owner  => $owner,
+    group  => $group,
     mode   => '0644',
   }
   
   file { 'docroot':
     ensure => directory,
-    path   => '/var/www',
+    path   => $docroot,
   }
   
   file { 'index.html':
-    path   => '/var/www/index.html',    
-    source => 'puppet:///modules/nginx/index.html,
+    path   => "${docroot}/index.html",    
+    source => 'puppet:///modules/nginx/index.html',
   }
   
   file { 'nginx.conf':
-    path   => '/etc/nginx/nginx.conf',
+    path   => "${confdir}/nginx.conf",
     source => 'puppet:///modules/nginx/nginx.conf',
   }
   
   file { 'default.conf':
-    path   => '/etc/nginx/conf.d/default.conf',
+    path   => "${blockdir}/default.conf",
     source => 'puppet:///modules/nginx/default.conf',
   }
   
