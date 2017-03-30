42b14f0d2985bae79339439032d9d0fe4c36af11
rwoodwar999 <rwoodwar@travelers.com>
2017-03-30 13:37:41 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,44 +1,62 @@
 class nginx (
-  String $root = undef,
-  Boolean $highperf = true,
-  ) {
-  case $facts['os']['family'] {
-    'redhat','debian' : {
-      $package  = 'nginx'
-      $owner    = 'root'
-      $group    = 'root'
-   #   $docroot  = '/var/www'
-      $confdir  = '/etc/nginx'
-      $blockdir = '/etc/nginx/conf.d' 
-      $logdir   = '/var/log/nginx'
-      $svcname  = 'nginx'
-      $runsas   = 'nginx'
-    }
-    default : {
-      fail("Module ${module_name} is not supported on ${facts['os']['family']}")
-    }
-  }
- $user = 'nginx'
- 
- # if $root isn't set, then fall back to the platform default
-  $docroot = $root ? {
-    undef => $default_docroot,
-    default => $root,
-    }
-  File {
-    owner => $owner,
-    group => $group,
-    mode => '0664',
-  }
-  package { $package:
-    ensure => present,
-  }
- 
-  file { "${docroot}/index.html":
-    ensure => file,
-    #source => 'puppet:///modules/nginx/index.html',
-    content => epp('nginx/index.html.epp'),
-  }
+String $root = undef,
+Boolean $highperf = true,
+) {
+case $facts['os']['family'] {
+'redhat','debian' : {
+$package = 'nginx'
+$owner = 'root'
+$group = 'root'
+# $docroot = '/var/www'
+$confdir = '/etc/nginx'
+$blockdir = '/etc/nginx/conf.d'
+$logdir = '/var/log/nginx'
+# this will be used if we don't pass in a value
+$default_docroot = '/var/www'
+}
+'windows' : {
+$package = 'nginx-service'
+$owner = 'Administrator'
+$group = 'Administrators'
+# $docroot = 'C:/ProgramData/nginx/html'
+$confdir = 'C:/ProgramData/nginx'
+$blockdir = 'C:/ProgramData/nginx/conf.d'
+$logdir = 'C:/ProgramData/nginx/logs'
+# this will be used if we don't pass in a value
+$default_docroot = 'C:/ProgramData/nginx/html'
+}
+default : {
+fail("Module ${module_name} is not supported on ${facts['os']['family']}")
+}
+}
+# user the service will run as. Used in the nginx.conf.epp template
+$user = $facts['os']['family'] ? {
+'redhat' => 'nginx',
+'debian' => 'www-data',
+'windows' => 'nobody',
+}
+# if $root isn't set, then fall back to the platform default
+$docroot = $root ? {
+undef => $default_docroot,
+default => $root,
+}
+File {
+owner => $owner,
+group => $group,
+mode => '0664',
+}
+package { $package:
+ensure => present,
+}
+# docroot is either passed in or a default value
+nginx::vhost { 'default':
+docroot => $docroot,
+servername => $facts['fqdn'],
+}
+file { "${docroot}/vhosts":
+ensure => directory,
+}
+
   file { "${confdir}/nginx.conf":
     ensure => file,
   #  source => "puppet:///modules/nginx/${facts['os']['family']}.conf",
